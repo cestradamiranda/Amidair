f57c4e0c90395cfabd5c437838b1e5964f2799b9
unknown <cestrada.miranda@gmail.com>
2017-08-12 14:19:37 -0400

    weather service use lodash to travers json object

--- src/services/mirabelConditions.js
+++ src/services/mirabelConditions.js
@@ -1,6 +1,7 @@
 //  api.openweathermap.org/data/2.5/weather?q=Mirabel,ca&APPID=40b692ad8785628b93f95e632d031111
 import React, { Component } from 'react';
 import '../App.css'
+import get from 'lodash/get'
 
 class MirabelConditions extends Component {
   constructor(props) {
@@ -20,16 +21,21 @@ class MirabelConditions extends Component {
         this.setState({
           myData: json
         })
-        console.log("dfdsfd" + json);
+        console.log( "Log: " + json);
       })   
 
   };
       
   render() {
     //c'ant work with objects inside render
+    //get values from json with lodash _ 
     return (
     <div className="App-mirabel">
-           <div>Name: {this.state.myData.name} </div>
+           <div> Area: {get(this.state.myData, 'name')} </div>
+           <div> Sky: {get(this.state.myData, 'weather[0].main')} </div>
+           <div> Wind Speed: {get(this.state.myData, 'wind.speed')} </div>
+           <div> Wind Degree: {get(this.state.myData, 'wind.deg')} </div>
+
     </div>
     );
   }
